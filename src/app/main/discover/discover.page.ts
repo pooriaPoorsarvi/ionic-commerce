44b492d26705631d9d678942b2a382217e4f6f49
poori <pooriapoorsarvi@gmail.com>
2020-05-08 01:36:26 -0400

    main page initialised

--- src/app/main/discover/discover.page.ts
+++ src/app/main/discover/discover.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { setInterval } from 'timers';
 
 @Component({
   selector: 'app-discover',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscoverPage implements OnInit {
 
+  @ViewChild(IonSlides, {static: true}) ionSlides: IonSlides;
+  goingForward = true;
+  // The ends : -1 : first, 0 : none, 1 : last
+  endThatHasBeenReached = 0;
+  slideLoopTime = 2000;
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    autoHeight: true
+  };
+
   constructor() { }
 
   ngOnInit() {
+    setInterval(
+      () => {
+        this.moveSlide();
+      }
+      , this.slideLoopTime);
+    this.ionSlides.ionSlideReachEnd.subscribe(
+      () => {
+        this.endThatHasBeenReached = 1;
+      }
+    );
+    this.ionSlides.ionSlideReachStart.subscribe(
+      () => {
+        this.endThatHasBeenReached = -1;
+      }
+    );
+  }
+
+  checkDirection() {
+    if (this.goingForward) {
+      this.goingForward = this.endThatHasBeenReached !== 1;
+    } else {
+      this.goingForward = this.endThatHasBeenReached === -1;
+    }
+    this.endThatHasBeenReached = 0;
+  }
+  moveSlide() {
+    this.checkDirection();
+    if (this.goingForward) {
+      this.ionSlides.slideNext();
+    } else {
+      this.ionSlides.slidePrev();
+    }
+
   }
 
 }
